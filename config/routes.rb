10007431b82a5e3eed2af018b95a8db640db0bce
Armando Avello <av_dsm90@hotmail.com>
2020-03-23 21:06:45 -0300

    creando controlador

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'test/index'
+  get 'test/about'
+  get 'test/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
